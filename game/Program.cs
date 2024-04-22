105ae251874ac2209fb162a94ead6a3db4ab3524
tempor4ry <muradisrafilzade97@gmail.com>
2024-04-22 18:17:19 +0400

    Add project files.

---
+++ game/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+string duzgunSoz = "Baku";
+int haqq = 5;
+
+Console.WriteLine("Duzgun cavabi daxil edin:");
+string cavab = Console.ReadLine();
+while (haqq > 5)
+{
+    if (cavab == duzgunSoz)
+    {
+        Console.WriteLine("Tebrikler cavab dogrudur");
+        break;
+    }
+    else
+    {
+        haqq--;
+        Console.WriteLine($"Cavab sehfdir. Qalan haqqinizin say; {haqq}");
+        Console.WriteLine("Bir daha sinayin");
+        cavab = Console.ReadLine();
+    }
+
+    if (haqq == 0)
+    {
+        Console.WriteLine("Meqlub oldunuz");
+    }
+    Console.ReadLine();
+
+}
